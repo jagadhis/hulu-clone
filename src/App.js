3d2ba6654d80e4aceceb04c3bca0fc2a6772c14b
jagadhis <jwaran78@gmail.com>
2021-10-10 10:28:41 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>lets build a hulu clone🔥</h1>
+    </div>
+  );
+}
+
+export default App;
